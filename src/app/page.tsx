12154603ccad8138b6fca708c675c8650e2e2cf9
davignz <werdavid1@gmail.com>
2024-10-02 20:50:44 -0300

    feat: estilização inicial home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,29 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export default function Home(){
-
-  return(
-    <div>
-      <div>
-        <h1>CP2 - Worlds in Colision</h1>
-        <p>Este projeto apresenta o CheckPoint 2 de Frontend Design Engineering</p>
-        <Image src="/img/worlds_in_colision.jpg" alt="capa livro" width={50} height={50} 
-      priority={true}/>
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex flex-col items-center justify-center p-5 gap-4">
+      <div className="w-full h-auto flex flex-col items-center justify-center gap-2">
+        <h2 className="font-bold text-2xl">CP2 - Worlds in Colision</h2>
+        <p>
+          Este projeto apresenta o CheckPoint 2 de Frontend Design Engineering
+        </p>
+        <Image
+          src="/img/worlds_in_colision.jpg"
+          alt="Capa do livro Worlds in Collision"
+          width={300}
+          height={50}
+          priority={true}
+        />
       </div>
-      <div>
+      <div className="w-full h-full flex flex-col items-center justify-center">
         <ul>
+          <h3 className="text-center font-bold">Integrantes</h3>
           <li>André Marcolongo - RM 555285</li>
           <li>Davi Gonzaga Ferreira - RM 554890</li>
           <li>Leandro Correia - RM 556203</li>
         </ul>
       </div>
-    </div>
-  )
+    </main>
+  );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-10 flex items-center justify-center absolute bottom-0">
+    <footer className="w-full h-10 flex items-center justify-center relative bottom-0">
       <p>Worlds in Collision &copy; Checkpoint</p>
     </footer>
   );
