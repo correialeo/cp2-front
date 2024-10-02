8204bf385d8e11226e578626cf904b9784ce4d98
correialeo <leandroo774@outlook.com>
2024-10-02 20:32:22 -0300

    parametros do get alterados para date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image"
+
 export default function Home(){
+
   return(
     <div>
-      <h1>homepage</h1>
+      <Image src="/img/worlds_in_colision.jpg" alt="capa livro" width={50} height={50} 
+      priority={true}/>
     </div>
   )
 }
