7e77f96400b61493b4de59618e03fe227bd161e6
correialeo <leandroo774@outlook.com>
2024-10-02 20:40:19 -0300

    html da homepage estruturado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,19 @@ export default function Home(){
 
   return(
     <div>
-      <Image src="/img/worlds_in_colision.jpg" alt="capa livro" width={50} height={50} 
+      <div>
+        <h1>CP2 - Worlds in Colision</h1>
+        <p>Este projeto apresenta o CheckPoint 2 de Frontend Design Engineering</p>
+        <Image src="/img/worlds_in_colision.jpg" alt="capa livro" width={50} height={50} 
       priority={true}/>
+      </div>
+      <div>
+        <ul>
+          <li>André Marcolongo - RM 555285</li>
+          <li>Davi Gonzaga Ferreira - RM 554890</li>
+          <li>Leandro Correia - RM 556203</li>
+        </ul>
+      </div>
     </div>
   )
 }
